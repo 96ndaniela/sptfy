b752cfa808b4aef07451fad9e4b34c3f7d6d950a
dani <75045898+96ndaniela@users.noreply.github.com>
2025-05-18 16:53:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,24 @@ def load_data():
     }
     return spotify_df, user_files
 
+def main():
+    st.title("spotify: song recommendations (based on tempo & ranking")
+    sptfy_df, user_files = load_data()
+
+    st.sidebar.header("select user, or upload a new csv")
+    user_choice = st.sidebar.selectbox("choose an user playlist:", list(user_files.keys()) + ["upload new"])
+
+    if user_choice == "upload new":
+        uploaded_file = st.sidebar.file_uploader("upload your playlist in csv form", type=["csv"])
+        if uploaded_file is not None:
+            user_df = pd.read_csv(uploaded_file)
+            user_name = "new user uploaded!"
+        else:
+            st.info("please upload a new csv.")
+            st.stop()
+    else:
+        user_name = user_choice
+        user_df = pd.read_csv(user_files[user_choice])
+
 if __name__ == "__main__":
     main()
